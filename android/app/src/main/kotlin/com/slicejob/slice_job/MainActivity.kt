2180a25252084e75199c3fbce740070e4bee2d4d
Purushottam Parajuli <purushottam@fawesomeapps.com>
2023-01-08 08:33:38 +0545

    android plugins update

--- android/app/src/main/kotlin/com/slicejob/slice_job/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.slicejob.slice_job
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/slicejob/slice_job/MainActivity.kt
@@ -0,0 +1,6 @@
+package slicejob.slice_job
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+}
